90bf4b46182de13ef5bfe53995fa538b626577d5
Tamires Dib <tamiresdib6@gmail.com>
2024-08-19 21:55:27 -0300

    feat: exercice annotations and interfaces

---
+++ index.ts
@@ -0,0 +1,26 @@
+const input = document.querySelector("input");
+
+const total = localStorage.getItem("total");
+if (input && total) {
+  input.value = total;
+  calcularGanho(Number(input.value));
+}
+
+function calcularGanho(value: number) {
+  const p = document.querySelector("p");
+  if (p) {
+    p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+  }
+}
+
+function totalMudou() {
+  if (input) {
+    localStorage.setItem("total", input.value);
+    calcularGanho(Number(input.value));
+    console.log("tamissss");
+  }
+}
+
+if (input) {
+  input.addEventListener("keyup", totalMudou);
+}
